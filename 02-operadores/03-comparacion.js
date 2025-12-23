3f580c6209ea642d01892411630bc93d76cf4552
Ramos <andfera201@gmail.com>
2025-12-23 07:54:15 -0500

    comparacion

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10
+// Relacionales
+console.log(a > 5)
+console.log(a >= 5)
+console.log(a < 5)
+console.log(a <= 5)
+
+// Igualdad
+console.log(a == 5)
+console.log(a != 5)
+console.log(a == '5')
+console.log(a === '5')
+console.log(a !== 5)
