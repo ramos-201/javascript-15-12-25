e1005221a277e0d6b11ea262d275f7b48d805f67
Ramos <andfera201@gmail.com>
2026-01-05 08:21:03 -0500

    else

--- 03-control-flujo/02-else.js
+++ 03-control-flujo/02-else.js
@@ -2,7 +2,10 @@ let age = 10
 
 if (age > 17) {
     console.log("es mayor de edad")
-} else {
+} else if (age > 13) {
+    console.log("Es menor de edad pero necesita estar acompañado")
+}
+else {
     console.log("es menor de edad")
 }
 
