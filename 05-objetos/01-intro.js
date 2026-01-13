1a2acfd53246a55fdfac4c2e7bae7345ae0ae34d
Ramos <andfera201@gmail.com>
2026-01-12 21:39:01 -0500

    objectos intr

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,21 @@
+let email = "and@gmail.com"
+let name = "Andres"
+let dir = {
+    calle: "...",
+    numero: "..."
+}
+
+let user = {
+    email: "and@gmail.com",
+    name: "Andres",
+    dir: {
+        calle: "...",
+        numero: "...",
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log("Recuperar clave")
+    },
+}
+
+
